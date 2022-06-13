d8ec4c81fce3583327bf4df60f12b2d44921fe2f
Dang Tling <20010908@st.phenikaa-uni.edu.vn>
2022-06-13 12:11:13 +0700

    finsh-project

---
+++ admin_area/includes/db.php
@@ -0,0 +1,5 @@
+<?php
+
+$con = mysqli_connect("localhost","root","tuanlinh26022002","web_ban_quan_ao");
+mysqli_set_charset($con, "utf8");
+?>
---
+++ includes/db.php
@@ -0,0 +1,5 @@
+<?php
+
+$con = mysqli_connect("localhost","root","tuanlinh26022002","web_ban_quan_ao");
+
+?>
